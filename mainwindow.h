c740c7805673c2a8655fd6b17632e2f17ddc9b52
jihen111 <131627835+jihen111@users.noreply.github.com>
2023-11-01 11:16:00 +0100

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "Mission.h"
+#include <QMessageBox>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,19 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+
+    void on_pushButtonAjoutMission_clicked();
+
+    void on_pushButtonSupprimerMission_clicked();
+
+    void on_pushButtonModifierMission_clicked();
+    void on_tableViewMission_clicked(const QModelIndex &index);
+
 private:
     Ui::MainWindow *ui;
+    Mission Atmp;
 };
 
 #endif // MAINWINDOW_H
+
