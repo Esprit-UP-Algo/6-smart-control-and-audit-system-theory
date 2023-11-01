bc91722111d246005cb49595c200d5eafeef868c
jihen111 <131627835+jihen111@users.noreply.github.com>
2023-11-01 11:12:15 +0100

    Update connection.cpp

--- connection.cpp
+++ connection.cpp
@@ -8,16 +8,12 @@ Connection::Connection()
 bool Connection::createconnect()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-db.setDatabaseName("mybase");
-db.setUserName("maroua");//inserer nom de l'utilisateur
-db.setPassword("maroua");//inserer mot de passe de cet utilisateur
+db.setDatabaseName("Source_Projet2A.");
+db.setUserName("jihen");//inserer nom de l'utilisateur
+db.setPassword("221JFT6667");//inserer mot de passe de cet utilisateur
 
 if (db.open())
 test=true;
-
-
-
-
 
     return  test;
 }
